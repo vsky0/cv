2fd16970fd0c59cb7ecb5a19499ea51d18929f06
Velpula Sai Kiran Yadav <saikiranyadavvelpula.001@gmail.com>
2024-09-20 18:15:47 +0530

    added change_resolution function

--- rescale_resize/rescaling.py
+++ rescale_resize/rescaling.py
@@ -11,6 +11,9 @@ def rescaled(frame, scale=0.75):
 # video
 
 def rescale_video():
+    """
+    use for rescaling videos.
+    """
     capture = cv.VideoCapture('data/videos/wikipedia_donate.mp4')
 
     while True:
@@ -36,4 +39,44 @@ def rescale_image():
     cv.imshow('original image', image)
     cv.waitKey(0)
 
-rescale_image()
+# change the resolution of the live video.
+def change_resolution(width, height):
+    """
+    this only works on the live video.
+    """
+    capture = cv.VideoCapture(0)
+    capture.set(3, width)
+    capture.set(4, height)
+
+    while True:
+        is_true, frame = capture.read()
+        cv.imshow('Changed Resolution', frame)
+
+        if cv.waitKey(20) & 0xFF==ord('d'):
+            break
+    
+    capture.release()
+    cv.destroyAllWindows()
+
+if __name__ == '__main__':
+    print("Choose an option:")
+    print("1 for rescaling image.")
+    print("2 for rescaling video.")
+    print("3 for changing resolution for video.")
+
+    choice = int(input("Enter your choice:"))
+
+    if choice == 1:
+        print("YOU'VE SELECTED THE RESCALING IMAGE.")
+        print("YOU WILL SEE BOTH ORIGINAL AND RESCALED IMAGES.")
+
+        rescale_image()
+    elif choice == 2:
+        print("YOU'VE SELECTED THE RESCALING VIDEO.")
+        print("YOU WILL SEE BOTH ORIGINAL AND RESCALED VIDEOS.")
+        rescale_video()
+    elif choice == 3:
+        print("YOU WILL SEE CHANGED RESOLUTION OF LIVE VIDEO.")
+        change_resolution(10, 10)
+    else:
+        print("INVALID CHOICE")
