2208469fe6c1a2bc3ab6a54f4a3a9db406c1a26c
Velpula Sai Kiran Yadav <saikiranyadavvelpula.001@gmail.com>
2024-09-20 17:02:50 +0530

    be cautious about large images

--- show_img.py
+++ show_img.py
@@ -5,3 +5,6 @@ img = cv2.imread('data/photos/cell.jpg')
 cv2.imshow('Cell', img)
 
 cv2.waitKey(0)
+
+# for images whose size is greater thab the size of the window size,
+# image is shown big covering more than size of window.
