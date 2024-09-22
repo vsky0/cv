a22a5c175c4b9f9bb321b5c58d6c7d3ae3a597f0
Velpula Sai Kiran Yadav <saikiranyadavvelpula.001@gmail.com>
2024-09-22 10:22:56 +0530

    changed image to see reduction in no.of contours on blurred image.

--- contour_detection/contour.py
+++ contour_detection/contour.py
@@ -4,21 +4,28 @@ import cv2 as cv
 # in the image, contour detection is used in object detection,
 # image recognition
 
-img = cv.imread('computer_vision/data/photos/geometric_shapes.jpg')
+img = cv.imread('computer_vision/data/photos/bonsai_tree.jpg')
 
 # first convert the image into gray
 
 gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('gray image', gray_img)
 
-# second find the edges, of the image, doing using canny
+# blur the image, then pass it to the canny input
+blur_img = cv.GaussianBlur(img, (5,5), cv.BORDER_DEFAULT)
+cv.imshow('blur image', blur_img)
+
+# find the edges, of the image, doing using canny
 
 img_edges = cv.Canny(img, 125, 175) # here the 125, 175 are the threshold values.
 cv.imshow('edges in the image', img_edges)
 
 # now apply findContours method to img_edges
 
-contours, hierarchies = cv.findContours(img_edges, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-print(f"the number of countours find={len(contours)}")
+contours, hierarchies = cv.findContours(img_edges, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+print(f"the number of contours in image={len(contours)}")
 
 cv.waitKey(5000)
+
+# applying blur to images reduces noise and provides only significant contors, while unblurred image 
+# can have more contours.
