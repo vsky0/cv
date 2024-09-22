95c75f03d805ae963e0fc23215cf17e2af868a1e
Velpula Sai Kiran Yadav <saikiranyadavvelpula.001@gmail.com>
2024-09-22 10:13:01 +0530

    find contours

---
+++ contour_detection/contour.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+
+# contours are the borders/boundaries of the objects
+# in the image, contour detection is used in object detection,
+# image recognition
+
+img = cv.imread('../data/photos/geometric_shapes.jpg')
+
+# first convert the image into gray
+
+gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('gray image', gray_img)
+
+# second find the edges, of the image, doing using canny
+
+img_edges = cv.Canny(img, 125, 175) # here the 125, 175 are the threshold values.
+cv.imshow('edges in the image', img_edges)
+
+# now apply findContours method to img_edges
+
+contours, hierarchies = cv.findContours(img_edges, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+print(f"the number of countours find={len(contours)}")
