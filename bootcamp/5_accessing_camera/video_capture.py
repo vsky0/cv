28e74ded6b025d92941e0ea96a834ec3a883cee3
Velpula Sai Kiran Yadav <saikiranyadavvelpula.001@gmail.com>
2024-09-23 22:16:11 +0530

    access camera for video capture

---
+++ bootcamp/5_accessing_camera/video_capture.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+import sys
+
+s = 0 # for the default camera
+
+# check if any input source is provided while executing the program
+if len(sys.argv)>1:
+    s = sys.argv[1]
+
+soruce = cv.VideoCapture(s)
+
+window_name = 'camera'
+cv.namedWindow(window_name, cv.WINDOW_NORMAL)
+
+while cv.waitKey(1) != 27: #escape
+    has_frame,frame = source.read()
+    if not has_frame:
+        break
+    cv.imshow(window_name,frame)
+
+source.release()
+cv.destroyAllWindows(window_name)
